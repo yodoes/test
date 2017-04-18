f38c0ab5430ed36a9bd00688e5d709b32ceff3ba
lvhaifeng <1326804259@qq.com>
2017-04-18 14:23:57 +0800

    delete files

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by admin on 2017/4/18.
- */
-var gulp = require('gulp');
-
-gulp.task('default',function(){
-   console.log('hello');
-});
--- readme.md
+++
@@ -1,2 +0,0 @@
-## gulp环境配置
-+ 安装node、npm ,下载gulp插件。
